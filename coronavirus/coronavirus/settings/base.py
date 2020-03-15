4be8be3cc568bb5c975b85f6a4809a0d80592ed1
Jose Ramirez <jcramireztello@gmail.com>
2020-03-15 18:19:24 +0100

    feat: Agregando swagger y json:api

--- coronavirus/coronavirus/settings/base.py
+++ coronavirus/coronavirus/settings/base.py
@@ -112,3 +112,40 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# rest framework
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 100,
+    'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework_json_api.pagination.JsonApiPageNumberPagination',
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_json_api.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_json_api.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer'
+    ),
+    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_json_api.filters.QueryParameterValidationFilter',
+        'rest_framework_json_api.filters.OrderingFilter',
+        'rest_framework_json_api.django_filters.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ),
+    'SEARCH_PARAM': 'filter[search]',
+    'TEST_REQUEST_RENDERER_CLASSES': (
+        'rest_framework_json_api.renderers.JSONRenderer',
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json',
+}
+
+JSON_API_FORMAT_FIELD_NAMES = 'camelize'
+
+SWAGGER_SETTINGS = {
+    'DEFAULT_AUTO_SCHEMA_CLASS': 'drf_yasg_json_api.view_inspectors.SwaggerJSONAPISchema'
+}
--- coronavirus/coronavirus/swagger.py
+++ coronavirus/coronavirus/swagger.py
@@ -5,9 +5,18 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="REPORTE DE CORONAVIRUS API",
+      title="API - Casos confirmados en el mundo de COVID-19",
       default_version='v1',
-      description="API rest para el reporte de coronavirus"
+      description="""
+      API rest de reporte de casos confirmados en el mundo de coronavirus COVID-19 presentados en 
+      [Coronavirus COVID-19 Global Cases by the Center for Systems Science and Engineering (CSSE) 
+      at Johns Hopkins University (JHU)](
+      https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6)
+      
+      __Autor:__ [Jose Ramirez](https://www.linkedin.com/in/jcramireztello/)
+      
+      _esta api utiliza la especificacion [JSON:API](https://jsonapi.org/)_
+      """,
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
